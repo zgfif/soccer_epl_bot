57f9f0b6260026a896aef0764074c240d88f0a08
zgfif <zihzag@gmail.com>
2019-02-18 09:31:05 +0200

    set parses for result

---
+++ last_result.rb
@@ -0,0 +1,38 @@
+require 'nokogiri'
+require 'open-uri'
+
+class LastResult
+  attr_reader :site
+
+  def initialize
+    @site = 'https://football.ua/england.html'
+  end
+
+  def perform
+   match_time + ' ' + home_team + ' ' + score + ' ' + guest_team
+  end
+
+  def page
+    @page ||= Nokogiri::HTML(open(site))
+  end
+
+  def match_time
+    page.at_css('td.time').text
+  end
+
+  def home_team
+    page.at_css('td.left-team').text
+  end
+
+  def score
+    unhandled_score.gsub(/\n|\r/, "").strip
+  end
+
+  def guest_team
+    page.at_css('td.right-team').text
+  end
+
+  def unhandled_score
+    page.at_css('td.score.ended a').text
+  end
+end
--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require 'telegram/bot'
+require_relative 'last_result'
 
 TOKEN = ENV['TELEGRAM_TOKEN']
 
@@ -16,6 +17,11 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
       bot.api.send_message(
         chat_id: message.chat.id,
         text: "Bye, #{message.from.first_name}"
+      )
+    when '/result'
+      bot.api.send_message(
+        chat_id: message.chat.id,
+        text: "first result of EPL #{LastResult.new.perform}"
       )
     else
       bot.api.send_message(
