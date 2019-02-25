44f03fd2592bb66b08c33f69e0bd8afcb7a8409a
zgfif <zihzag@gmail.com>
2019-02-25 21:42:01 +0200

    #8: add help command

--- main.rb
+++ main.rb
@@ -3,6 +3,15 @@ require_relative 'last_result'
 
 TOKEN = ENV['TELEGRAM_TOKEN']
 
+def help_text
+  "Commands: \n
+  /start - show specific keyboard;\n
+  /england - show English Premier League result;\n
+  /ukraine - show Ukrainian Premier League result;\n
+  /help - list of available commands.\n
+  /stop - hide specific keyboard."
+end
+
 Telegram::Bot::Client.run(TOKEN, logger: Logger.new($stderr)) do |bot|
   bot.logger.info('Bot has been started')
 
@@ -13,9 +22,13 @@ Telegram::Bot::Client.run(TOKEN, logger: Logger.new($stderr)) do |bot|
 
       bot.api.send_message(chat_id: message.chat.id, text: 'Привет, какой результат интересует?', reply_markup: kb)
     when '/stop'
-      bot.api.send_message(chat_id: message.chat.id, text: 'Bye-bye')
+      hide_kb = Telegram::Bot::Types::ReplyKeyboardRemove.new(remove_keyboard: true)
+
+      bot.api.send_message(chat_id: message.chat.id, text: 'До встречи', reply_markup: hide_kb)
     when '/england'
       bot.api.send_message(chat_id: message.chat.id, text: LastResult.new.perform)
+    when '/help'
+      bot.api.send_message(chat_id: message.chat.id, text: help_text)
     end
   end
 end
